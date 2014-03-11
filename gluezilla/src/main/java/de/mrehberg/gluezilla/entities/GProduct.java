fb3c644890f789918a1694a2bd05fc5e87cb498d
Sascha Vogt <sascha@vogt-neuenbuerg.de>
2014-03-11 17:50:05 +0100

    Remove unused imports

--- gluezilla/src/main/java/de/mrehberg/gluezilla/entities/GProduct.java
+++ gluezilla/src/main/java/de/mrehberg/gluezilla/entities/GProduct.java
@@ -19,6 +19,7 @@ package de.mrehberg.gluezilla.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
